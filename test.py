9539ca3f91f463690df49ae613f84d7321b4e75b
Kitton._ <tonkittipod@gmail.com>
2022-04-15 23:01:58 +0700

    11 12

--- test.py
+++ test.py
@@ -1,14 +1,5 @@
-import numpy as np
+import pandas as pd
 
-array = np.array([
-    [3, 2, -6, -3, -6],
-    [-3, -5, 1, 3, -5],
-    [3, 2, 2, -1, 2],
-    [-3, 1, -5, -1, -3],
-])
-# [[3, 2],
-#  [-3, -5],
-#  [3, 2],
-#  [-3, 1], ]
-array_t = np.transpose(array)
-print(array_t[[1, 2]])
+df = pd.read_csv("./sportsday.csv")
+
+print()
